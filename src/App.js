cfbc1c3d8bc9d536d7f0dad1b96edd3225f7349a
Nick <ndondzila@gmail.com>
2020-06-20 08:11:33 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './ft-logo.png';
 import './App.css';
+import Auxiliary from './hoc/Auxiliary';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Auxiliary>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>**A demo project not created/deployed by company**</p>
+          </header>
+      </Auxiliary>
+      <br />
+      <Auxiliary>
+          <label className="App-label">Please enter an IP address or domain to check:</label>
+          <br />
+          <hr/>
+          <input type='text' className="App-input"></input>
+        </Auxiliary>
     </div>
   );
 }
